977c54cabe463f68468c13ba57ade95048912237
Ilja <rav@Anastasias-MacBook-Pro.local>
2024-05-11 23:00:07 +0300

    added more images

--- public/tiletypes/powerProducers/renewable/Tidal.js
+++ public/tiletypes/powerProducers/renewable/Tidal.js
@@ -2,6 +2,6 @@ import { PowerProducer } from "../PowerProducer.js";
 
 export class Tidal extends PowerProducer {
     constructor(scene, i, j) {
-        super(scene, i, j, 'blue', 170, true); // 170 is an example power output
+        super(scene, i, j, 'tidal');
     }
 }
--- public/tiletypes/terrain/Forest.js
+++ public/tiletypes/terrain/Forest.js
@@ -2,6 +2,6 @@ import { Tile } from "../../Tile.js";
 
 export class Forest extends Tile {
     constructor(scene, i, j) {
-        super(scene, i, j, 'green');
+        super(scene, i, j, 'forest');
     }
 }
